01add1305715a815866348e6840a02b49b939358
“Priscila” <prifcampos@gmail.com>
2026-05-02 18:17:13 -0300

    Aula 03 Angular

--- src/app/components/lista-produtos/lista-produtos.ts
+++ src/app/components/lista-produtos/lista-produtos.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CardProduto } from '../card-produto/card-produto';
 import { Product } from '../../services/product';
 import { Cart } from '../../services/cart';
@@ -9,14 +9,20 @@ import { Cart } from '../../services/cart';
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
-export class ListaProdutos {
+export class ListaProdutos implements OnInit {
   // Injeção de dependência moderna
   product = inject(Product);
   cartService = inject(Cart);
-  produtos = this.product.getProducts();
+  produtos: any[] = [];
+
+  ngOnInit(): void {
+    console.log('funcionou')
+    this.produtos = this.product.getProducts();
+  }
 
   receberProduto(produto: any) {
     console.log("Produto adicionado: ", produto.title);
+    // Simula a chama de API
     this.cartService.adicionar(produto);
   }
 }
